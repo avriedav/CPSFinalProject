17724c8475bf39877f2dc0470a3cda561a031929
avriedav <118912541+avriedav@users.noreply.github.com>
2022-11-23 10:35:05 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,26 +1,45 @@
 import java.util.*;
 public class Test {
 	public static void main(String[] args) {
+		System.out.println("Hello!");
+		System.out.println("Welcome to Time Conversion Calcuator!");
 		Scanner input = new Scanner(System.in);
-		System.out.print("Please select your friend's continent from the following options: "
-				+ "North America, South America, Asia, Europe, Africa, and Australia\n");
-		String Continent = input.nextLine();
+		System.out.print("Please select your continent from the following options: "
+				+ "\n1 for North America, 2 for South America, 3 for Asia, 4 for Europe, 5 for Africa, and 6 for Australia\n");
+		int userContinent = input.nextInt();
 		
-		while (Continent == "North America") {
-			//print graph of the continent
-			Scanner input2 = new Scanner(System.in);
-			System.out.print("Please enter your local time! What is the hour?");
-			int Hour = input.nextInt();
+		switch (userContinent) {
+		case 1: 
+			System.out.println("Your continent is North America! (:");
+			Scanner input1 = new Scanner(System.in);
+			System.out.print("Please select your friend's continent from the following options: "
+					+ "\n1 for North America, 2 for South America, 3 for Asia, 4 for Europe, 5 for Africa, and 6 for Australia\n");
+			int friendContinent = input.nextInt();
 			
-			Scanner input3 = new Scanner(System.in);
-			System.out.print("Please enter the minute: ");
-			int Minute = input.nextInt();
-			
-			Scanner input4 = new Scanner(System.in);
-			System.out.print("Please enter the day: ");
-			String Day = input.nextLine();
-			
-			System.out.println("Your local time is " + Hour + ":" + Minute + " " + "on " + Day);
+			switch (friendContinent) {
+			case 1: 
+				System.out.println("Your friend's continent is North America! (:");
+				System.out.println("Your local time will be based on military time.");
+				Scanner input2 = new Scanner(System.in);
+				System.out.print("Please enter the hour for your local time: ");
+				int hour = input.nextInt();
+				
+				Scanner input3 = new Scanner(System.in);
+				System.out.print("Please enter the minute: ");
+				int minute = input.nextInt();
+				
+				System.out.println("What day of the week is it?");
+				Scanner input4 = new Scanner(System.in);
+				System.out.print("Enter 1 for Monday, 2 for Tuesday, 3 for Wednesday, and so on...");
+				int day = input. nextInt();
+				
+				switch (day) {
+				case 1:
+					
+					System.out.print("You said your local time is " + hour + ":"  + minute + " on Monday");	
+				}
+		
+			}
 		}
 	}
 }
